96deaf42274cef68a9b116f2cecb00264d9610d6
Bongibault Romain <bongibault.romain@gmail.com>
2023-07-23 00:47:37 +0200

    fix: render items

--- src/main/java/fr/codecrafters/infiniteutopia/block/entity/renderer/CuttingBoardEntityRenderer.java
+++ src/main/java/fr/codecrafters/infiniteutopia/block/entity/renderer/CuttingBoardEntityRenderer.java
@@ -1,6 +1,7 @@
 package fr.codecrafters.infiniteutopia.block.entity.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import fr.codecrafters.infiniteutopia.block.CuttingBoard;
 import fr.codecrafters.infiniteutopia.block.entity.CuttingBoardEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LightTexture;
@@ -11,6 +12,7 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -30,16 +32,28 @@ public class CuttingBoardEntityRenderer implements BlockEntityRenderer<CuttingBo
     public void render(CuttingBoardEntity pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
         ItemStack itemStack = pBlockEntity.getItem();
 
-        for (int i = 0; i < itemStack.getCount(); i++) {
-            renderItem(itemStack, pPoseStack, pBlockEntity, pBuffer, i + 1);
+        for (int i = 0; i < Math.min(itemStack.getCount(), 4); i++) {
+            renderItem(itemStack, pPoseStack, pBlockEntity, pBuffer, i);
         }
     }
 
     private void renderItem(ItemStack itemStack, PoseStack pPoseStack, CuttingBoardEntity entity, MultiBufferSource pBuffer, int position) {
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
 
+        int dx = 0, dz = 0;
+
+        switch (entity.getBlockState().getValue(CuttingBoard.FACING)) {
+            case NORTH, SOUTH -> dx = 1;
+            case EAST, WEST -> dz = 1;
+        }
+
         pPoseStack.pushPose();
-        pPoseStack.translate(0.5, (0.2 + (0.01 * position)), 0.5);
+        if (position == 0) {
+            pPoseStack.translate(dx == 0 ? 0.40 : dx * 0.25, (0.08), dz == 0 ? 0.40 : dz * 0.25);
+        } else {
+            pPoseStack.translate(dx == 0 ? 0.40 : dx * 0.6, (0.08 + (0.03 * (position - 1))), dz == 0 ? 0.40 : dz * 0.6);
+        }
+
         pPoseStack.scale(0.6f, 0.6f, 0.6f);
         pPoseStack.rotateAround(new Quaternionf(new AxisAngle4f((float) (Math.PI/2), 1, 0, 0)), 0F, 0F, 0F);
 
